40472ff0cae0e2bb794a550c4343de181444e5f9
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2021-06-11 10:42:37 +0200

    Create Grandi_magazzini.R

---
+++ Grandi_magazzini.R
@@ -0,0 +1,10 @@
+install.packages("xlsx")
+install.packages("readxl")
+
+library(tidyverse)
+library(googlesheets4)
+library(xlsx)
+library(readxl)
+
+Supermercati_Ita_2019 <- read_excel("Tav_3_SM.xls") %>%
+  mutate(tipo="SM")
